5053105329b14bc31aec4bb6be512ef45773480b
thiago <thiagon16@hotmail.com>
2025-04-22 21:52:43 -0300

    feat: create header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header className="w-full bg-[#7e1e2a] flex items-center shadow-md shadow-zinc-400 justify-center">
+      <h1 className="font-bold text-4xl py-6 text-white">
+        Controle de Estoque de Vinhos
+      </h1>
+    </header>
+  );
+}
