77d5d5e94a177fc8550e197d87e90d23fd5484c2
thiago <thiagon16@hotmail.com>
2025-04-23 23:05:44 -0300

    style: change break points on menu navigation and sidebar

--- src/components/sidebar/menu-mobile.tsx
+++ src/components/sidebar/menu-mobile.tsx
@@ -11,7 +11,7 @@ import { Navigation } from "./navigation";
 export function MenuMobile() {
   return (
     <Sheet>
-      <SheetTrigger className="sm:hidden">
+      <SheetTrigger className="md:hidden">
         <Menu className="text-white" />
       </SheetTrigger>
       <SheetContent className="px-5 pt-6" side="left">
